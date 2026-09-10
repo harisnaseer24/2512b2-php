6693aa4320f3774c8f1886f481bf019e73ba4379
Haris Naseer <harisnaseer@aptech.com>
2026-09-10 09:25:40 +0500

    add products done

---
+++ ecommerce/admin/addproduct.php
@@ -0,0 +1,121 @@
+<?php 
+include_once("./components/header.php");
+include_once("../config/connection.php");
+
+
+?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="page-header">
+            <h3 class="page-title">
+              Add Product
+            </h3>
+          </div>
+          <div class="row grid-margin">
+           <!-- form -->
+           
+           
+            <div class="col-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Enter product details</h4>
+                  
+                  <form class="forms-sample" action="" method="post">
+                    <div class="form-group">
+                      <label for="title">Title</label>
+                      <input type="text"  required class="form-control" name="title"id="title" placeholder="Enter product title">
+                    </div>
+                    
+
+                    <div class="form-group">
+                      <label for="price">price</label>
+                      <input type="number"  required class="form-control" name="price"id="title" placeholder="Enter product price">
+                    </div>
+                    <div class="form-group">
+                      <label for="stock">stock</label>
+                      <input type="number" required class="form-control" name="stock"id="stock" placeholder="Enter product stock">
+                    </div>
+                           <div class="form-group">
+                      <label for="image">image</label>
+                      <input type="text" required class="form-control" name="image"id="image" placeholder="Enter product image">
+                    </div>
+                    
+                    <div class="form-group">
+                      <label for="cat_id">Category</label>
+                        <select class="form-control" required name="cat_id" id="exampleSelectGender">
+                          <option selected disabled>Select Category</option>
+                          <option value="1">Men</option>
+                          <option value="2">Women</option>
+                        </select>
+                      </div>
+                    <!-- <div class="form-group">
+                      <label>File upload</label>
+                      <input type="file" name="img[]" class="file-upload-default">
+                      <div class="input-group col-xs-12">
+                        <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                        <span class="input-group-append">
+                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                        </span>
+                      </div>
+                    </div> -->
+                   
+                    <div class="form-group">
+                      <label for="description">Description</label>
+                      <textarea class="form-control" required name="description" id="description" rows="4"></textarea>
+                    </div>
+                    <button type="submit" name="addproduct" class="btn btn-primary mr-2">Submit</button>
+                    <button class="btn btn-light">Cancel</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+           <!-- form -->
+
+          </div>
+        
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="far fa-heart text-danger"></i></span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+  <?php 
+include_once("./components/footer.php");
+
+
+if(isset($_POST['addproduct'])){
+
+$title= $_POST['title'];
+$price= $_POST['price'];
+$stock= $_POST['stock'];
+$image= $_POST['image'];
+$cat_id= $_POST['cat_id'];
+$description= $_POST['description'];
+
+$add = "INSERT INTO `products`( `title`, `description`, `price`, `stock`, `image`, `cat_id`) VALUES ('$title','$description','$price','$stock','$image','$cat_id')";
+
+$result= mysqli_query($conn, $add);
+if ($result) {
+  # code...
+  echo "<script>alert('Product added successfully')
+  window.location.href='./products.php'
+  </script>";
+} else {
+  # code...
+    echo "<script>alert('Failed to add product')</script>";
+}
+
+
+
+}
+
+
+
+?>
---
+++ ecommerce/database/2512b2-php.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2026 at 06:26 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `2512b2-php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(100) NOT NULL,
+  `cat_desc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_name`, `cat_desc`) VALUES
+(1, 'Men', 'All Products with good quality for men'),
+(2, 'Women', 'Good quality products for women');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `title`, `description`, `price`, `stock`, `image`, `cat_id`, `created_at`) VALUES
+(1, 'Jacket', 'dfdf', 9999, 120, 'jacket.png', 1, '2026-09-03 10:06:27'),
+(2, 'Scarf', 'abc', 1000, 500, 'scarf.png', 2, '2026-09-05 09:48:37');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`cat_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
